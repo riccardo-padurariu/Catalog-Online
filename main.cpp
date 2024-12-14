67a34a756b2aa430acfe0aab87b45a713e229884
Padurariu Riccardo <padurariuriccardo.github@gmail.com>
2024-12-14 19:56:43 +0200

    Implemented the grading system

--- main.cpp
+++ main.cpp
@@ -63,6 +63,7 @@ class Student:public User {
 private:
     double Grade;
     string Year,Status="";
+    vector<pair<int,string>> Grades[1001];
 public:
     //Student constructor
     Student(string name,string surname,string gender,string password,int id,int age,int grade,string year)
@@ -70,12 +71,6 @@ public:
             Grade = grade;
             Year = year;
         }
-    void setGrade(int grade){
-        Grade = grade;
-    }
-    double getGrade(){
-        return Grade;
-    }
     void setYear(string year){
         Year = year;
     }
@@ -83,13 +78,44 @@ public:
         return Year;
     }
     void setStatus(){
-        if(Grade>=5)
-            Status = "Promoted";
-        else
-            Status = "Failed";
+        if(Grade != 0){
+            if(Grade>=5)
+                Status = "Promoted";
+            else
+                Status = "Failded";
+        }else
+            Status = "Undefined";
     }
     string getStatus(){
         return Status;
+    }
+
+    double getGrade(){
+        return Grade;
+    }
+
+    void setGrade(int grade,string date,int index){
+        Grades[index].push_back({grade,date});
+        cout << "Grade added" << '\n';
+        for(auto x : Grades[index]){
+            cout << x.first << " ";
+        }
+        cout << '\n';
+    }
+
+    void displayStatistics(){
+        vector<string> subjects = {"Romanian","English","French","Religion","Math","Physics","History","Geography","Biology"};
+        for(int i=0;i<9;i++){
+            cout << subjects[i] << ": ";
+            if(Grades[i].size() == 0)
+                cout << "No grades" << '\n';
+            else{
+                for(auto j : Grades[i]){
+                    cout << j.first << "/" << j.second << " ";
+                }
+                cout << '\n';
+            }
+        }
     }
 
     void display(){
@@ -161,7 +187,7 @@ void addStudent(){
     for(int i=1;i<=number;i++){
         string name,surname,gender,password,year;
         int id,age;
-        double grade;
+        double grade=0;
         cout << "Enter name: ";
         cin >> name;
         cout << "Enter surname: ";
@@ -176,8 +202,6 @@ void addStudent(){
         cin >> id;
         cout << "Enter age: ";
         cin >> age;
-        cout << "Enter grade: ";
-        cin >> grade;
         Student s = Student(name,surname,gender,password,id,age,grade,year);
         s.setStatus();
         students.push_back(s);
@@ -301,6 +325,56 @@ void displayClasses(){
     }
 }
 
+/* Functions for the student management*/
+
+void findStudent(int id){
+    for(auto i : students){
+        if(i.getId() == id)
+            i.display();
+    }
+}
+
+void addGrade(int id){
+    int grade;
+    string date,subject;
+    cout << "Enter grade: ";
+    cin >> grade;
+    cout << "Enter date: ";
+    cin >> date;
+    cout << "Enter subject number: " << '\n';
+    cout << "0. Romanian" << '\n';
+    cout << "1. English" << '\n';
+    cout << "2. French" << '\n';
+    cout << "3. Religion" << '\n';
+    cout << "4. Math" << '\n';
+    cout << "5. Physics" << '\n';
+    cout << "6. History" << '\n';
+    cout << "7. Geography" << '\n';
+    cout << "8. Biology" << '\n';
+    int choice;
+    cin >> choice;
+    int index;
+    for(int i=0;i<students.size();i++){
+        if(students[i].getId() == id)
+            index = i;
+    }
+    cout << index << '\n';
+    students[index].setGrade(grade,date,choice);
+}
+
+void addAbsent(int id){
+
+}
+
+void displayStatisticsStudent(int id){
+    int index;
+    for(int i=0;i<students.size();i++){
+        if(students[i].getId() == id)
+            index = i;
+    }
+    students[index].displayStatistics();
+}
+
 int main()
 {
     cout << "Online Catalog Opened" << '\n';
@@ -313,7 +387,8 @@ int main()
         cout << "5. Add teachers" << '\n';
         cout << "6. Delete teachers" << '\n';
         cout << "7. Display classes" << '\n';
-        cout << "8. Close Catalog" << '\n';
+        cout << "8. Find student" << '\n';
+        cout << "9. Close Catalog" << '\n';
         int choice;
         cin >> choice;
         switch(choice){
@@ -339,8 +414,38 @@ int main()
             displayClasses();
             break;
         case 8:
+            int id;
+            cout << "Enter the id of the student: " << '\n';
+            cin >> id;
+            findStudent(id);
+            cout << "1. Add grade" << '\n';
+            cout << "2. Add absent" << '\n';
+            cout << "3. Display grades" << '\n';
+            cout << "4. Go back to main menu" << '\n';
+            int choice2;
+            cin >> choice2;
+            switch(choice2){
+            case 1:
+                addGrade(id);
+                continue;
+                break;
+            case 2:
+                addAbsent(id);
+                continue;
+                break;
+            case 3:
+                displayStatisticsStudent(id);
+                continue;
+                break;
+            case 4:
+                continue;
+                break;
+            }
+        case 9:
             cout << "Catalog Closed" << '\n';
             exit(0);
+        default:
+            cout << "Invalid choice" << '\n';
         }
     }
     return 0;
